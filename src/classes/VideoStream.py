2147f78a6d9624d798222d87c33adccbf419babf
Justin Miller <justinmiller24@gmail.com>
2020-01-02 13:22:15 -0600

    Unified interface for both picamera and USB/webcam video feeds

---
+++ src/classes/VideoStream.py
@@ -0,0 +1,44 @@
+# This class creates a unified interface to both picamera and cv2.VideoCapture
+# using a single class name. This allows us to seamlessly access either a
+# built-in/USB camera or the Raspberry Pi camera module without changing code.
+
+# TODO: increase webcam FPS by using threading
+# https://docs.opencv.org/3.4.6/d5/dae/tutorial_aruco_detection.html
+
+# Import the necessary packages
+from webcamvideostream import WebcamVideoStream
+
+class VideoStream:
+	def __init__(self, src=0, usePiCamera=False, resolution=(320, 240), framerate=32):
+		# check to see if the picamera module should be used
+		if usePiCamera:
+			# only import the picamera packages unless we are
+			# explicity told to do so -- this helps remove the
+			# requirement of `picamera[array]` from desktops or
+			# laptops that still want to use the `imutils` package
+			from pivideostream import PiVideoStream
+
+			# initialize the picamera stream and allow the camera
+			# sensor to warmup
+			self.stream = PiVideoStream(resolution=resolution, framerate=framerate)
+
+		# otherwise, we are using OpenCV so initialize the webcam
+		# stream
+		else:
+			self.stream = WebcamVideoStream(src=src)
+
+	def start(self):
+		# start the threaded video stream
+		return self.stream.start()
+
+	def update(self):
+		# grab the next frame from the stream
+		self.stream.update()
+
+	def read(self):
+		# return the current frame
+		return self.stream.read()
+
+	def stop(self):
+		# stop the thread and release any resources
+		self.stream.stop()
