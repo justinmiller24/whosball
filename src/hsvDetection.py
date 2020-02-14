21b36221e6b6bddf0e556c8a98f2c83a383c79cf
Justin Miller <justinmiller24@gmail.com>
2020-02-14 13:25:24 -0600

    Script to detect HSV values from center coordinate in video stream

---
+++ src/hsvDetection.py
@@ -0,0 +1,70 @@
+# USAGE
+# python hsvDetection.py
+
+# import the necessary packages
+from collections import deque
+from imutils.video import VideoStream
+import cv2
+import datetime
+import imutils
+import numpy as np
+import time
+
+
+vs = VideoStream(usePiCamera=True).start()
+
+# allow the camera or video file to warm up
+print("Warming up camera or video file")
+time.sleep(2.0)
+
+# keep looping
+while True:
+	# grab the current frame
+	frame = vs.read()
+
+	# handle the frame from VideoCapture or VideoStream
+	frame = frame[1] if args.get("video", False) else frame
+
+	# Find center
+	#x = int(frame.shape[1] / 2)
+	#y = int(frame.shape[0] / 2)
+
+	# Convert to HSV
+	hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+	(h, w) = hsv_img.shape[:2]
+	centerPX = mask[int(h/2), int(w/2)]
+    print centerPX
+
+	# Crop image
+	#cropped = image[70:170, 440:540]
+	#cv2.imshow("cropped", cropped)
+
+	# Resize, blur it, and convert to HSV
+	#frame = imutils.resize(frame, width=600)
+	#blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	#hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+	# Create "red" color mask, perform erosions and dilation to remove small blobs in mask
+	#mask = cv2.inRange(hsv, ballLower, ballUpper)
+	#mask = cv2.erode(mask, None, iterations=2)
+	#mask = cv2.dilate(mask, None, iterations=2)
+
+	# Find contours in mask and initialize the current center (x, y) of the ball
+	#cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	#cnts = imutils.grab_contours(cnts)
+	#center = None
+
+	# show the frame to our screen
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
+
+# stop the camera video stream
+vs.release()
+
+# close all windows
+cv2.destroyAllWindows()
