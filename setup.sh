9edb94a45c54e1c06f6006edbea86477a9e9cbfd
Justin Miller <justinmiller24@gmail.com>
2020-02-14 11:40:35 -0600

    Update OpenCV install in setup script

--- setup.sh
+++ setup.sh
@@ -19,8 +19,8 @@
 sudo apt update && sudo apt upgrade
 sudo raspi-config
 
-# Remove LibreOffice packages
-sudo apt remove --purge -y libreoffice*
+# Remove packages to reclaim ~1GB of space
+sudo apt purge -y wolfram-engine libreoffice*
 sudo apt clean && sudo apt -y autoremove
 
 # Set default python version to python3
@@ -37,28 +37,28 @@ pip3 install --upgrade imutils
 # INSTALL OPENCV        #
 #########################
 
+# Setup OpenCV
+# https://www.pyimagesearch.com/2019/09/16/install-opencv-4-on-raspberry-pi-4-and-raspbian-buster/
+
 # Install developer tools
 sudo apt install -y build-essential cmake pkg-config
 
-# Install image and video I/O packages (allows loading of various file formats from disk)
+# Install image I/O packages so we can read image file formats (JPEG, PNG, TIFF, etc)
 sudo apt install -y libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
-sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt install -y libxvidcore-dev libx264-dev
 
-# Install GTK development library
-sudo apt install -y libfontconfig1-dev libcairo2-dev
-sudo apt install -y libgdk-pixbuf2.0-dev libpango1.0-dev
-sudo apt install -y libgtk2.0-dev libgtk-3-dev
+# Install video I/O packages so we can read video file formats and work directly with video streams
+sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
 
-# Install optimization libraries (useful for Raspberry Pi)
+# Install GTK development library and prerequisites (so we can compile the highgui module)
+sudo apt install -y libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev
+
+# Install optimization libraries
 sudo apt install -y libatlas-base-dev gfortran
 
 # Install dependencies for opencv and so we can compile OpenCV with python bindings
-sudo apt install -y libhdf5-dev libhdf5-serial-dev libhdf5-103
-sudo apt install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
-sudo apt install -y python3-dev
+sudo apt install -y libhdf5-dev libhdf5-serial-dev libhdf5-103 libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 python3-dev
 
-# Install camera module
+# Install PiCamera API
 pip3 install --upgrade "picamera[array]"
 
 # Install OpenCV
@@ -67,7 +67,7 @@ pip3 install --upgrade opencv-contrib-python==4.1.0.25
 # Install missing dependencies
 # ImportError: libImath-2_2.so.12: cannot open shared object file: No such file or directory
 # ImportError: libIlmImf-2_2.so.22: cannot open shared object file: No such file or directory
-sudo apt install -y libilmbase23 libopenexr-dev
+#sudo apt install -y libilmbase23 libopenexr-dev
 
 
 
@@ -76,7 +76,7 @@ sudo apt install -y libilmbase23 libopenexr-dev
 #########################
 
 # Clean up unused packages
-sudo apt -y autoremove
+sudo apt clean && sudo apt -y autoremove
 
 # Python default version
 python --version
