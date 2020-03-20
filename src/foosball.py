75ce1cd47ab91b36f6d8c4cfb8b4246cd97c5cee
Justin Miller <justinmiller24@gmail.com>
2020-03-20 15:53:11 -0500

    Add file to determine motor response and action

---
+++ src/foosball.py
@@ -0,0 +1,12 @@
+
+
+def checkForScore():
+    print("Check to see if a score occurred")
+
+
+def determineMotorMovement():
+	print("Determine which motors to move")
+
+
+def moveMotors():
+    print("Move motors")
