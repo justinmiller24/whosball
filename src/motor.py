e83206fbe21f82875bec2ae1ef7ecd5cc6e9c303
Justin Miller <justinmiller24@gmail.com>
2020-03-24 16:45:36 -0500

    Fix spacing and indentation

--- src/motor.py
+++ src/motor.py
@@ -6,18 +6,18 @@ def determineMove():
 
 
 def move():
-    print("Move motors")
+	print("Move motors")
 
-    # Initialise the first hat on the default address
-    # Stepper motors are available as stepper1 and stepper2
-    # stepper1 is made up of the M1 and M2 terminals
-    # stepper2 is made up of the M3 and M4 terminals
-    #self.motors.kit1 = Motorkit().stepper1
-    #self.motors.kit2 = Motorkit().stepper2
+	# Initialise the first hat on the default address
+	# Stepper motors are available as stepper1 and stepper2
+	# stepper1 is made up of the M1 and M2 terminals
+	# stepper2 is made up of the M3 and M4 terminals
+	#self.motors.kit1 = Motorkit().stepper1
+	#self.motors.kit2 = Motorkit().stepper2
 
-    # Initialise the second hat on a different address
-    #self.motors.kit3 = MotorKit(address=0x61).stepper1
-    #self.motors.kit4 = MotorKit(address=0x61).stepper2
+	# Initialise the second hat on a different address
+	#self.motors.kit3 = MotorKit(address=0x61).stepper1
+	#self.motors.kit4 = MotorKit(address=0x61).stepper2
 
-    # https://learn.adafruit.com/adafruit-dc-and-stepper-motor-hat-for-raspberry-pi/using-stepper-motors
-    #self.motors.kit1.stepper1.onestep()
+	# https://learn.adafruit.com/adafruit-dc-and-stepper-motor-hat-for-raspberry-pi/using-stepper-motors
+	#self.motors.kit1.stepper1.onestep()
