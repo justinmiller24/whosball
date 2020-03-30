4c72f223ab88fc2b91f5e7c3b2a8685a688c37d5
Justin Miller <justinmiller24@gmail.com>
2020-03-30 11:43:52 -0500

    Update media and scripts for circle detection test

--- scripts/detectCircles.py
+++ scripts/detectCircles.py
@@ -4,6 +4,7 @@
 
 # This script is used to detect circles in an image
 # Created by Justin Miller on 3.20.2020
+# https://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
 
 # USAGE:
 # python3 detectCircles.py --image IMAGE_PATH
@@ -37,7 +38,7 @@ if circles is not None:
 
 		# draw the circle in the output image, then draw a rectangle
 		# corresponding to the center of the circle
-		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+		cv2.circle(output, (x, y), r, (0, 0, 255), 4)
 		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
 	# show the output image
