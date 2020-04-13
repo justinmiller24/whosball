6a4504ee45d4afaec05b3499ba3176d9b7879ebf
Justin Miller <justinmiller24@gmail.com>
2020-04-13 12:43:27 -0500

    Update default options and finalize comments

--- scripts/generateArucoMarkers.py
+++ scripts/generateArucoMarkers.py
@@ -7,8 +7,8 @@
 
 # USAGE:
 # python3 generateArucoMarkers.py
-# python3 generateArucoMarkers.py --rows 3 --columns 2
-# python3 generateArucoMarkers.py --rows 3 --columns 2 --output outputFile.png
+# python3 generateArucoMarkers.py --rows 4 --columns 3
+# python3 generateArucoMarkers.py --rows 4 --columns 3 --save
 
 # Import packages
 import argparse
@@ -18,20 +18,20 @@ import matplotlib.pyplot as plt
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-r", "--rows", type=int, default=3, help="Number of rows to generate")
-ap.add_argument("-c", "--columns", type=int, default=4, help="Number of columns to generate")
+ap.add_argument("-r", "--rows", type=int, default=2, help="Number of rows to generate")
+ap.add_argument("-c", "--columns", type=int, default=2, help="Number of columns to generate")
 ap.add_argument("-s", "--save", help="Save output to file", action="store_true")
 args = vars(ap.parse_args())
 
 arucoDict = aruco.Dictionary_get(aruco.DICT_4X4_50)
-print(arucoDict)
+#print(arucoDict)
 
 fig = plt.figure()
 for i in range(0, args["columns"] * args["rows"]):
 	ax = fig.add_subplot(args["rows"], args["columns"], i + 1)
 	# Draw ArUco marker
 	# The second parameter is the ID number, the third parameter is the total image size
-	img = aruco.drawMarker(arucoDict, i, 200)
+	img = aruco.drawMarker(arucoDict, i, 100)
 	plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
 	ax.axis("off")
 
