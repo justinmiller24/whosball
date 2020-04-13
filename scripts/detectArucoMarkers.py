435b5ee6b0b8430ca7931fd3faad9d87b9edb15e
Justin Miller <justinmiller24@gmail.com>
2020-04-13 12:47:47 -0500

    Update ArUco marker detection script

--- scripts/detectArucoMarkers.py
+++ scripts/detectArucoMarkers.py
@@ -1,115 +1,71 @@
-# Detection of ArUco markers
-# https://docs.opencv.org/3.4.6/d5/dae/tutorial_aruco_detection.html
-# https://dzone.com/articles/marker-tracking-via-websockets-with-raspberry-pi
+###########################
+# Detect ArUco markers    #
+###########################
 
-import asyncio
+# This script is used to detect ArUco markers
+# Created by Justin Miller on 4.13.2020
+
+# USAGE:
+# python3 detectArucoMarkers.py
+
+# Import packages
 import cv2
-import concurrent
-import logging
-import math
+import cv2.aruco as aruco
 import numpy as np
 
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 
-RESOLUTION = (1088,1088)
-FRAMERATE = 30
-
-DICTIONARY = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
-
-# pi cam
-CAMERA_MATRIX = np.array([[2.01976721e+03, 0.00000000e+00, 1.32299009e+03],[0.00000000e+00, 2.00413989e+03, 8.60071427e+02],[0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
-DIST_COEFFS = np.array([-0.63653859,  0.2963752,  0.03228459, 0.0028887, 0.82956323])
-
-PARAMETERS =  cv2.aruco.DetectorParameters_create()
-MARKER_EDGE = 0.05
-
-def angles_from_rvec(rvec):
-    r_mat, _jacobian = cv2.Rodrigues(rvec)
-    a = math.atan2(r_mat[2][1], r_mat[2][2])
-    b = math.atan2(-r_mat[2][0], math.sqrt(math.pow(r_mat[2][1],2) + math.pow(r_mat[2][2],2)))
-    c = math.atan2(r_mat[1][0], r_mat[0][0])
-    return [a,b,c]
-
-def calc_heading(rvec):
-    angles = angles_from_rvec(rvec)
-    degree_angle =  math.degrees(angles[2])
-    if degree_angle < 0:
-        degree_angle = 360 + degree_angle
-    return degree_angle
-
-def find_markers(frame):
+def findMarkers(frame, arucoDictionary, arucoParameters):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, DICTIONARY, parameters=PARAMETERS)
-    rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, MARKER_EDGE, CAMERA_MATRIX, DIST_COEFFS)
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, arucoDictionary, parameters=arucoParameters)
 
     result = set()
     if ids is None:
         return result
 
     for i in range(0, len(ids)):
-        try:
-            id = str(ids[i][0])
+        id = str(ids[i][0])
 
-            marker = np.squeeze(corners[i])
+        marker = np.squeeze(corners[i])
 
-            x0, y0 = marker[0]
-            x2, y2 = marker[2]
-            x = int((x0 + x2)/2)
-            y = int((y0 + y2)/2)
+        x0, y0 = marker[0]
+        x2, y2 = marker[2]
+        x = int((x0 + x2)/2)
+        y = int((y0 + y2)/2)
 
-            heading = calc_heading(rvecs[i][0])
-            result.add((id, x, y, heading))
-        except Exception:
-            traceback.print_exc()
+        result.add((id, x, y))
 
     return result
 
-class PositioningSystem:
-    def __init__(self, on_update):
-        self._on_update = on_update
 
-    def start(self):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(self._capture())
-        loop.run_forever()
+cap = cv2.VideoCapture(0)
 
-    async def _capture(self):
+while(True):
 
-        camera = PiCamera()
-        camera.resolution = RESOLUTION
-        camera.framerate = FRAMERATE
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    arucoDict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    arucoParameters =  aruco.DetectorParameters_create()
 
-        rawCapture = PiRGBArray(camera, size=RESOLUTION)
+    #lists of ids and the corners beloning to each id
 
-        asyncio.sleep(0.1)
+    # "markerCorners" is the list of corners of the detected markers.
+    # For each marker, its four corners are returned in their original order (which is clockwise starting with top left).
+    # So, the first corner is the top left corner, followed by the top right, bottom right and bottom left.
+    # "markerIds" is the list of ids of each of the detected markers in markerCorners.
+    # Note that the returned markerCorners and markerIds vectors have the same size.
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, arucoDict, parameters=arucoParameters)
+    print(corners)
 
-        logging.info("Start capturing from pi camera.")
-        try:
-            for capture in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    if len(ids) > 0:
+        #gray = aruco.drawDetectedMarkers(gray, corners)
+        frameMarkers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 
-                frame = capture.array
-                markers = find_markers(frame)
-                rawCapture.truncate(0)
+    # Display the resulting frame
+    cv2.imshow('frame', frameMarkers)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-                for marker in markers:
-                    await self._on_update(marker)
-
-        except:
-            logging.error("Capturing stopped with an error.")
-
-        finally:
-            camera.close()
-
-
-# Create a limited thread pool.
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-
-async def track(on_track):
-    ps = PositioningSystem(on_track)
-
-    loop = asyncio.get_event_loop()
-    loop.run_in_executor(executor, ps.start)
-    return ps
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
