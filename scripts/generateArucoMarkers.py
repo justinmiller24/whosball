c6e5d744ecc3d5debd743b7aedfbc152ba63a781
Justin Miller <justinmiller24@gmail.com>
2020-04-13 09:50:41 -0500

    Update comment and adjust output to optional parameter

--- scripts/generateArucoMarkers.py
+++ scripts/generateArucoMarkers.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 ap = argparse.ArgumentParser()
 ap.add_argument("-r", "--rows", type=int, default=3, help="Number of rows to generate")
 ap.add_argument("-c", "--columns", type=int, default=4, help="Number of columns to generate")
-ap.add_argument("-o", "--output", default="output.png", help="Path to output file")
+ap.add_argument("-o", "--output", help="Path to output file", action="store_true")
 args = vars(ap.parse_args())
 
 arucoDict = aruco.Dictionary_get(aruco.DICT_6X6_250)
@@ -29,14 +29,14 @@ print(arucoDict)
 fig = plt.figure()
 for i in range(1, args["columns"] * args["rows"] + 1):
 	ax = fig.add_subplot(args["rows"], args["columns"], i)
-
 	# Draw ArUco marker
-	# The second parameter is the ID number
-	# The third parameter is the total image size
+	# The second parameter is the ID number, the third parameter is the total image size
 	img = aruco.drawMarker(arucoDict, i, 700)
-
 	plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
 	ax.axis("off")
 
-plt.savefig(args["output"])
-plt.show()
+# Save output file or show output
+if args["output"]:
+	plt.savefig(args["output"])
+else:
+	plt.show()
