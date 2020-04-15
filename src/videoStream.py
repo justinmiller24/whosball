3b5d21995260874789baa115290047f2eea6562d
Justin Miller <justinmiller24@gmail.com>
2020-04-15 14:54:41 -0500

    Fix indentation

--- src/videoStream.py
+++ src/videoStream.py
@@ -7,7 +7,7 @@ from threading import Thread
 class videoStream:
 
     # Initialize
-	#def __init__(self, debug=False, resolution=(640, 480), framerate=32, videoFile=None, outputFile=None):
+    #def __init__(self, debug=False, resolution=(640, 480), framerate=32, videoFile=None, outputFile=None):
     def __init__(self, debug=False, resolution=(640, 480), framerate=32):
 
         self.debug = debug
@@ -20,12 +20,12 @@ class videoStream:
         #else:
             #self.stream = VideoStream(usePiCamera=self.usePiCamera).start()
 
-		# initialize the camera and stream
-		self.camera = PiCamera()
-		self.camera.resolution = resolution
-		self.camera.framerate = framerate
-		self.rawCapture = PiRGBArray(self.camera, size=resolution)
-		self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
+        # initialize the camera and stream
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
 
         # Record to video output file
         #if self.outputFile:
@@ -35,60 +35,60 @@ class videoStream:
             #fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
             #self.writer = cv2.VideoWriter(self.outputFile, fourcc, 30, (mvWidth, mvHeight), True)
 
-		# initialize the frame and the variable used to indicate
-		# if the thread should be stopped
-		self.frame = None
-		self.stopped = False
+        # initialize the frame and the variable used to indicate
+        # if the thread should be stopped
+        self.frame = None
+        self.stopped = False
 
 
     # Start stream
-	def start(self):
-		# start the thread to read frames from the video stream
-		Thread(target=self.update, args=()).start()
-		return self
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
 
 
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		for f in self.stream:
-			# grab the frame from the stream and clear the stream in
-			# preparation for the next frame
-			self.frame = f.array
-			self.rawCapture.truncate(0)
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        for f in self.stream:
+            # grab the frame from the stream and clear the stream in
+            # preparation for the next frame
+            self.frame = f.array
+            self.rawCapture.truncate(0)
 
-			# if the thread indicator variable is set, stop the thread
-			# and resource camera resources
-			if self.stopped:
-				self.stream.close()
-				self.rawCapture.close()
-				self.camera.close()
+            # if the thread indicator variable is set, stop the thread
+            # and resource camera resources
+            if self.stopped:
+                self.stream.close()
+                self.rawCapture.close()
+                self.camera.close()
                 # Stop video/camera stream
                 #if self.videoFile is not None:
                     #self.stream.release()
                 #else:
                     #self.stream.stop()
 
-                # Stop recording video file
-                #if self.outputFile:
-                    #self.writer.release()
+            # Stop recording video file
+            #if self.outputFile:
+                #self.writer.release()
             return
 
 
     # Return the frame most recently used
-	def read(self):
+    def read(self):
         #self.frame = self.stream.read()
         #self.frame = self.frame[1] if self.videoFile is not None else self.frame
         #if self.frame is not None:
             #self.frame = imutils.resize(self.frame, width=600)
-		return self.frame
+        return self.frame
 
 
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
 
 
-    # Write output frame
-    #def write(self, output):
+        # Write output frame
+        #def write(self, output):
         #if self.writer is not None:
             #self.writer.write(output)
