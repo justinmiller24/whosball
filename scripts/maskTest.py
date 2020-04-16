a21f3ac3e9b12b5df21c583cc691c011db4f6827
Justin Miller <justinmiller24@gmail.com>
2020-04-16 16:52:35 -0500

    Update bug in test script

--- scripts/maskTest.py
+++ scripts/maskTest.py
@@ -17,13 +17,13 @@ mask = cv2.zeros(camera.resolution, cv2.CV_8UC1)
 #bitwise_not(mask, mask);
 mask = cv2.bitwise_not(mask)
 
-mask(cv2.Range(0, mask.rows), cv2.Range(0, 9 * mask.cols / 240 )) = 0
-mask(cv2.Range(0, mask.rows / 3), cv2.Range( 9 * mask.cols / 240 , 33 * mask.cols / 240)) = 0
-mask(cv2.Range(41 * mask.rows / 60, mask.rows), cv2.Range( 9 * mask.cols / 240,  33 * mask.cols / 240)) = 0
-mask(cv2.Range(0, mask.rows), cv2.Range(33 * mask.cols / 240, mask.cols / 6)) = 0
-mask(cv2.Range(0, mask.rows), cv2.Range(mask.cols / 4, 9 * mask.cols / 24)) = 0
-mask(cv2.Range(0, mask.rows), cv2.Range(29 * mask.cols / 60, 73 * mask.cols / 120)) = 0
-mask(cv2.Range(0, mask.rows), cv2.Range(3 * mask.cols / 4, mask.cols)) = 0
+mask[cv2.Range(0, mask.rows), cv2.Range(0, 9 * mask.cols / 240 )] = 0
+mask[cv2.Range(0, mask.rows / 3), cv2.Range( 9 * mask.cols / 240 , 33 * mask.cols / 240)] = 0
+mask[cv2.Range(41 * mask.rows / 60, mask.rows), cv2.Range( 9 * mask.cols / 240,  33 * mask.cols / 240)] = 0
+mask[cv2.Range(0, mask.rows), cv2.Range(33 * mask.cols / 240, mask.cols / 6)] = 0
+mask[cv2.Range(0, mask.rows), cv2.Range(mask.cols / 4, 9 * mask.cols / 24)] = 0
+mask[cv2.Range(0, mask.rows), cv2.Range(29 * mask.cols / 60, 73 * mask.cols / 120)] = 0
+mask[cv2.Range(0, mask.rows), cv2.Range(3 * mask.cols / 4, mask.cols)] = 0
 
 # Show display until a key is pressed
 mask3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
