efaf2ea5f947b11e3366032248b8d774b0a0f910
Justin Miller <justinmiller24@gmail.com>
2020-04-17 15:24:27 -0500

    Clean up code

--- scripts/maskTest.py
+++ scripts/maskTest.py
@@ -11,42 +11,27 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
 import numpy as np
+import time
 
 
 w = 960
 h = 640
-#cv::Mat mask = cv::Mat::zeros( size, CV_8UC1 );
-#bitwise_not(mask, mask);
-#whiteFrame = 255 * np.ones((w,h,3), np.uint8)
-#blurred = cv2.GaussianBlur(whiteFrame, (11, 11), 0)
-#hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
 
 mask = np.zeros((h, w, 3), dtype="uint8")
-#mask = np.ones((w,h,3), np.uint8)
+#mask = np.ones((h, w, 3), dtype="uint8")
 mask = cv2.bitwise_not(mask)
 
 #mask[cv2.Range(0, mask.rows), cv2.Range(0, 9 * mask.cols / 240 )] = 0
-#mask[0:w,0:9] = 0
 cv2.rectangle(mask, (0, 0), (w * 9 / 240, h), (0, 0, 0), -1)
-
 #mask[cv2.Range(0, mask.rows / 3), cv2.Range( 9 * mask.cols / 240 , 33 * mask.cols / 240)] = 0
 #mask[cv2.Range(41 * mask.rows / 60, mask.rows), cv2.Range( 9 * mask.cols / 240,  33 * mask.cols / 240)] = 0
-
 #mask[cv2.Range(0, mask.rows), cv2.Range(33 * mask.cols / 240, mask.cols / 6)] = 0
-#mask[0:w,33:40] = 0
 cv2.rectangle(mask, (w * 33 / 240, 0), (w * 40 / 240, h), (0, 0, 0), -1)
-
 #mask[cv2.Range(0, mask.rows), cv2.Range(mask.cols / 4, 9 * mask.cols / 24)] = 0
-#mask[0:w,60:90] = 0
 cv2.rectangle(mask, (w * 60 / 240, 0), (w * 90 / 240, h), (0, 0, 0), -1)
-
 #mask[cv2.Range(0, mask.rows), cv2.Range(29 * mask.cols / 60, 73 * mask.cols / 120)] = 0
-#mask[0:w,116:146] = 0
 cv2.rectangle(mask, (w * 116 / 240, 0), (w * 146 / 240, h), (0, 0, 0), -1)
-
 #mask[cv2.Range(0, mask.rows), cv2.Range(3 * mask.cols / 4, mask.cols)] = 0
-#mask[0:w,180:240] = 0
 cv2.rectangle(mask, (w * 180 / 240, 0), (w, h), (0, 0, 0), -1)
 
 
