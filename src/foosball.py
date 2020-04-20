d40fdf533997da2637366c988e679d2067cbac12
Justin Miller <justinmiller24@gmail.com>
2020-04-20 15:36:57 -0500

    Fix typo

--- src/foosball.py
+++ src/foosball.py
@@ -325,7 +325,7 @@ class foosball:
         self.mask3 = cv2.cvtColor(self.mask2, cv2.COLOR_GRAY2BGR)
 
         self.maskTest = cv2.erode(self.mask, None, iterations=3)
-        self.maskTest = cv2.dialate(self.maskTest, None, iterations=3)
+        self.maskTest = cv2.dilate(self.maskTest, None, iterations=3)
         self.maskTest = cv2.cvtColor(self.maskTest, cv2.COLOR_GRAY2BGR)
 
         # Find contours in mask and initialize the current center (x, y) of the ball
