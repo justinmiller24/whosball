c18fd6394f7ca588d4a4287e9131422a02a60007
Justin Miller <justinmiller24@gmail.com>
2020-04-21 13:53:16 -0500

    Fix array iterator

--- scripts/maskTest.py
+++ scripts/maskTest.py
@@ -27,7 +27,7 @@ mask = np.zeros((h, w, 3), dtype="uint8")
 mask = cv2.bitwise_not(mask)
 
 maskRED = mask
-for i in len(foosmenRED):
+for i in foosmenRED:
 	cv2.rectangle(maskRED, (foosmenRED[i] - foosmenWIDTH, 0), (foosmenRED[i] + foosmenWIDTH, h), (0, 0, 0), -1)
 #cv2.rectangle(mask, (int(w * 40 / 240), 0), (int(w * 70 / 240), h), (0, 0, 0), -1)
 #cv2.rectangle(mask, (int(w * 110 / 240), 0), (int(w * 150 / 240), h), (0, 0, 0), -1)
