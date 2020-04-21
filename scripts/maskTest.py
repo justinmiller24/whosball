e9c5b7c7950c3674fb4af43e06d7085edcdc42bb
Justin Miller <justinmiller24@gmail.com>
2020-04-21 13:32:29 -0500

    Add logic and update hstack display

--- scripts/maskTest.py
+++ scripts/maskTest.py
@@ -14,47 +14,18 @@ import numpy as np
 import time
 
 
-# Crop image to just the foosball table
-tL = (73,130)
-tR = (557,136)
-bR = (561,414)
-bL = (59,405)
+w = 640
+h = 480
 
-w = 960
-h = 640
-
-mask = np.zeros((h, w, 3), dtype="uint8")
-#mask = np.ones((h, w, 3), dtype="uint8")
-mask = cv2.bitwise_not(mask)
-
-#mask[cv2.Range(0, mask.rows), cv2.Range(0, 9 * mask.cols / 240 )] = 0
+# Create "mask" for RED foosball player
+#mask = np.zeros((h, w, 3), dtype="uint8")
+#mask = cv2.bitwise_not(mask)
+mask = np.ones((h, w, 3), dtype="uint8")
 cv2.rectangle(mask, (0, 0), (int(w * 9 / 240), h), (0, 0, 0), -1)
-#mask[cv2.Range(0, mask.rows / 3), cv2.Range( 9 * mask.cols / 240 , 33 * mask.cols / 240)] = 0
-#mask[cv2.Range(41 * mask.rows / 60, mask.rows), cv2.Range( 9 * mask.cols / 240,  33 * mask.cols / 240)] = 0
-#mask[cv2.Range(0, mask.rows), cv2.Range(33 * mask.cols / 240, mask.cols / 6)] = 0
 cv2.rectangle(mask, (int(w * 33 / 240), 0), (int(w * 40 / 240), h), (0, 0, 0), -1)
-#mask[cv2.Range(0, mask.rows), cv2.Range(mask.cols / 4, 9 * mask.cols / 24)] = 0
 cv2.rectangle(mask, (int(w * 60 / 240), 0), (int(w * 90 / 240), h), (0, 0, 0), -1)
-#mask[cv2.Range(0, mask.rows), cv2.Range(29 * mask.cols / 60, 73 * mask.cols / 120)] = 0
 cv2.rectangle(mask, (int(w * 116 / 240), 0), (int(w * 146 / 240), h), (0, 0, 0), -1)
-#mask[cv2.Range(0, mask.rows), cv2.Range(3 * mask.cols / 4, mask.cols)] = 0
 cv2.rectangle(mask, (int(w * 180 / 240), 0), (w, h), (0, 0, 0), -1)
-
-
-#mask1 = cv2.inRange(self.hsv, self.dim["foosballMin1HSV"], self.dim["foosballMax1HSV"])
-#mask2 = cv2.inRange(self.hsv, self.dim["foosballMin2HSV"], self.dim["foosballMax2HSV"])
-#mask = cv2.bitwise_or(mask1, mask2)
-#mask = cv2.erode(mask, None, iterations=2)
-#mask = cv2.dilate(mask, None, iterations=2)
-#self.mask3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-
-
-# Show display until a key is pressed
-#print(mask)
-
-#mask3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-cv2.imshow("Mask", mask)
-cv2.waitKey(0)
 
 
 # initialize the camera and stream
@@ -69,35 +40,50 @@ time.sleep(2.0)
 
 # loop over some frames
 for (i, f) in enumerate(stream):
-	# grab the frame from the stream
-	origFrame = f.array
-	#frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
+	# Grab latest frame from PiCamera stream
+	origFrame = f.array
+	img1 = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+	# Apply homography to show just the foosball table
+	# Crop image to just the foosball table
+	tL = (73,130)
+	tR = (557,136)
+	bR = (561,414)
+	bL = (59,405)
 	origCoords = np.array([tL, tR, bR, bL], dtype="float32")
 	finalCoords = np.array([(0,0), (w-1,0), (w-1,h-1), (0,h-1)], dtype="float32")
 	M = cv2.getPerspectiveTransform(origCoords, finalCoords)
 	frame = cv2.warpPerspective(origFrame, M, (w, h))
 
-	# display frame to our screen
-	# wait for keypress
-	cv2.imshow("Frame", frame)
-	cv2.waitKey(0)
-
-
-	#src2 = cv2.imread('data/src/horse_r.png')
 	mask2 = cv2.resize(mask, mask.shape[1::-1])
-
-	print(mask2.shape)
+	#print(mask2.shape)
 	# (225, 400, 3)
-	print(mask2.dtype)
+	#print(mask2.dtype)
 	# uint8
 
-	output = cv2.bitwise_and(frame, mask2)
-	#cv2.imwrite('data/dst/opencv_bitwise_and.jpg', dst)
 
-	# display frame to our screen
-	# wait for keypress
-	cv2.imshow("Output", output)
+	#img2 = contours_img
+
+	# Create a mask of contours and create its inverse mask also
+	#img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+	#ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+	#mask_inv = cv2.bitwise_not(mask)
+
+	# Now black-out the area of contours in original image
+	#img1_bg = cv2.bitwise_and(img1, img1, mask = mask_inv)
+
+	# Take only region of contours from contours image.
+	#img2_fg = cv2.bitwise_and(img2, img2, mask = mask)
+
+	# Put contours in image and modify the main image
+	#dst = cv2.add(img1_bg,img2_fg)
+	#cv2.imshow('res',dst)
+
+	output = cv2.bitwise_and(frame, mask2)
+
+	# Display output and wait for keypress
+	cv2.imshow("Output", np.hstack((mask, frame, output)))
 	cv2.waitKey(0)
 
 	# clear the stream in preparation for the next frame
