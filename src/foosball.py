a44704c845ed556b870f0d3a5f693b251c8a01e4
Justin Miller <justinmiller24@gmail.com>
2020-11-01 15:57:19 -0600

    Update foosball rod positions

--- src/foosball.py
+++ src/foosball.py
@@ -73,11 +73,11 @@ class Foosball:
             # The first rod is located at 38.91px + (rodWidth / 2) = 43.185px
             # Each additional rod is located an additional "rodSpacing" (79.09px) apart
             #'rowPosition': [43.19, 122.28, 201.37, 280.46, 359.55, 438.64, 517.73, 596.82],
-            'rowPosition': [32, 114, 196, 280, 360, 444, 526, 608],
+            'rowPosition': [28, 114, 198, 278, 362, 442, 526, 612],
             #'foosmenRED': np.array([43.19, 122.28, 280.46, 438.64], dtype="float32"),
-            'foosmenRED': np.array([32, 114, 280, 444], dtype="float32"),
+            'foosmenRED': np.array([28, 114, 278, 442], dtype="float32"),
             #'foosmenBLUE': np.array([201.37, 359.55, 517.73, 596.82], dtype="float32"),
-            'foosmenBLUE': np.array([196, 360, 526,608], dtype="float32"),
+            'foosmenBLUE': np.array([198, 362, 526, 612], dtype="float32"),
 
             # The foosmen are centered on each rod, but each rod (row) has a different number of men
             # There are 13 total foosmen, each with a unique ID from left to right and top to bottom
