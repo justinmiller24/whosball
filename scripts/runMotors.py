8ef372785f455d6e5edfbcbb6e4f9fe0aabe3988
Justin Miller <justinmiller24@gmail.com>
2020-11-30 16:05:19 -0600

    Clean up motor test script

--- scripts/runMotors.py
+++ scripts/runMotors.py
@@ -5,6 +5,7 @@
 # This script is used to test stepper motors with the TB6600 driver
 # Created by Justin Miller on 11.30.2020
 # https://www.instructables.com/Raspberry-Pi-Python-and-a-TB6600-Stepper-Motor-Dri/
+# https://learn.adafruit.com/adafruit-dc-and-stepper-motor-hat-for-raspberry-pi/powering-motors
 
 # USAGE:
 # python3 runMotors.py
@@ -21,8 +22,8 @@
 #Turn a 200 step motor left one full revolution: 200
 
 # Import packages
-from time import sleep
-import RPi.GPIO as gpio
+import RPi.GPIO as io
+import time
 
 # Global variables
 STEPS_PER_REVOLUTION = 200  # The number of steps needed for one full revolution
@@ -32,31 +33,35 @@ STP = 22    # Step
 DIR = 23    # Direction
 ENA = 24    # Enable
 
-# Use broadcom layout for the GPIO
-gpio.setmode(gpio.BCM)
-gpio.setup(STP, gpio.OUT)
-gpio.setup(DIR, gpio.OUT)
-gpio.setup(ENA, gpio.OUT)
+# Use broadcom pin-numbering scheme for GPIO pins
+# These pin numbers follow the lower-level numbering system defined by the Raspberry Pi's Broadcom-chip brain
+io.setmode(io.BCM)
+
+# Set pin modes for 3 GPIO pins used
+# Options = [GPIO.IN, GPIO.OUT]
+io.setup(STP, io.OUT)
+io.setup(DIR, io.OUT)
+io.setup(ENA, io.OUT)
 
 # Set direction (CW = true, CCW = false)
-#gpio.output(DIR, True)
-gpio.output(DIR, 1)
+#io.output(DIR, True)
+io.output(DIR, 1)
 
 # Start motor - power IS going to the motor
-#gpio.output(ENA, True)
-gpio.output(ENA, 1)
+#io.output(ENA, True)
+io.output(ENA, 1)
 
 # Main Loop
 for x in range(STEPS_PER_REVOLUTION):
-    #GPIO.output(STP, gpio.HIGH)
-    GPIO.output(STP, 1)
+    #io.output(STP, io.HIGH)
+    io.output(STP, 1)
     time.sleep(DELAY)
-    GPIO.output(STP, 0)
+    io.output(STP, 0)
     time.sleep(DELAY)
     print("Move one step")
 
 # Stop motor - power is NOT going to the motor
-gpio.output(ENA, 0)
+io.output(ENA, 0)
 
 # Clear GPIO settings
-gpio.cleanup()
+io.cleanup()
